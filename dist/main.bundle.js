e6ee2c3e01459889238cdc7a71e07c7aa8bd4748
Hsu Fu Chien <fuchien@br.ibm.com>
2017-12-07 10:59:13 -0200

    Change API url to https

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -557,17 +557,17 @@ var FirstFormService = (function () {
     }
     FirstFormService.prototype.pegarCanais = function () {
         // return this._http.get(`http://www.mocky.io/v2/5a291f9d2e0000b01ba09a50`)
-        return this.http.get("http://www.mocky.io/v2/5a291f9d2e0000b01ba09a50")
+        return this.http.get("https://www.mocky.io/v2/5a291f9d2e0000b01ba09a50")
             .map(function (res) { return res.json(); });
     };
     FirstFormService.prototype.pegarFilas = function () {
         // return this._http.get(`http://www.mocky.io/v2/5a2917352e00002e17a09a18`)
-        return this.http.get("http://www.mocky.io/v2/5a2917352e00002e17a09a18")
+        return this.http.get("https://www.mocky.io/v2/5a2917352e00002e17a09a18")
             .map(function (res) { return res.json(); });
     };
     FirstFormService.prototype.pegarDados = function () {
         // return this._http.get(`http://www.mocky.io/v2/5a2920912e0000021ca09a54`)
-        return this.http.get("http://www.mocky.io/v2/5a2920912e0000021ca09a54")
+        return this.http.get("https://www.mocky.io/v2/5a2920912e0000021ca09a54")
             .map(function (res) { return res.json(); });
         // if (numero.length <= 8)
         //   return this._http.get(`http://localhost:3000/pessoas/?pernumper=${numero}`)
--- src/app/home/first-form/first-form.service.ts
+++ src/app/home/first-form/first-form.service.ts
@@ -15,21 +15,21 @@ export class FirstFormService {
   pegarCanais() {
 
     // return this._http.get(`http://www.mocky.io/v2/5a291f9d2e0000b01ba09a50`)
-    return this.http.get(`http://www.mocky.io/v2/5a291f9d2e0000b01ba09a50`)
+    return this.http.get(`https://www.mocky.io/v2/5a291f9d2e0000b01ba09a50`)
     .map(res => res.json())
   }
 
   pegarFilas() {
 
     // return this._http.get(`http://www.mocky.io/v2/5a2917352e00002e17a09a18`)
-    return this.http.get(`http://www.mocky.io/v2/5a2917352e00002e17a09a18`)
+    return this.http.get(`https://www.mocky.io/v2/5a2917352e00002e17a09a18`)
       .map(res => res.json())
   }
 
   pegarDados() {
 
     // return this._http.get(`http://www.mocky.io/v2/5a2920912e0000021ca09a54`)
-    return this.http.get(`http://www.mocky.io/v2/5a2920912e0000021ca09a54`)
+    return this.http.get(`https://www.mocky.io/v2/5a2920912e0000021ca09a54`)
       .map(res => res.json())
     // if (numero.length <= 8)
     //   return this._http.get(`http://localhost:3000/pessoas/?pernumper=${numero}`)
